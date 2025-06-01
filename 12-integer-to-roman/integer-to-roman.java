9ac8548fb054894c0bd9ecab7c6354c1ace551b9
Samser Ali Khan <111233312+SamserAliKhan@users.noreply.github.com>
2025-06-01 20:55:54 +0530

    Time: 13 ms (6.50%) | Memory: 45.3 MB (8.40%) - LeetSync

---
+++ 12-integer-to-roman/integer-to-roman.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String intToRoman(int num) {
+        String[] ones = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+        String[] tens = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+        String[] hund = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+        String[] thus = {"", "M", "MM", "MMM"};
+        
+        int n=num,d=0;
+
+        int thousand = num/1000;
+        int hundred = (num/100)%10;
+        int ten = (num/10)%10;
+        int one = num%10;
+        while(n!=0){
+            n/=10;
+            d++;
+        }
+        String result;
+        if(d==4)result = thus[thousand] + hund[hundred ] + tens[ten] + ones[one];
+        else if(d==3)result = hund[hundred ] + tens[ten] + ones[one];
+        else if(d==2)result = tens[ten] + ones[one];
+        else result = ones[one];
+
+        return result;
+
+    }
+}
