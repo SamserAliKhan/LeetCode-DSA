0c63bd2d3de8529884261dbfef61c546a19613c2
Samser Ali Khan <111233312+SamserAliKhan@users.noreply.github.com>
2025-08-03 11:21:20 +0530

    Time: 7 ms (38.14%) | Memory: 71.7 MB (5.25%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -10,21 +10,37 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        //Brute Force approch using stack
-        if(head.next==null || head == null)return true;    
-        Stack <Integer> st = new Stack<>();
-        ListNode ptr = head;
-        int temp;
-        while(ptr!=null){
-            st.push(ptr.val);
-            ptr = ptr.next;
+        if(head == null || head.next == null)return true;
+        //Optimal Sollution using fast and slow then reversing the second half.
+        //Then checking form the head of the reversed half and the original half.
+        ListNode slow=head;
+        ListNode fast=head;
+        //finding the end of the first half
+        while(fast.next != null && fast.next.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
         }
-        ptr = head;
-        while(ptr!=null){
-            temp = st.pop();
-            if(temp!=ptr.val)return false;
-            ptr = ptr.next;
+        //now slow is pointing to the exact middle node of the LL
+        //reversing the second half of the linked list
+        ListNode newHead=reverse(slow.next);
+
+        //comparing the 2 halfs of the linedlist
+        while(newHead != null){
+            if(newHead.val != head.val)return false;
+            newHead = newHead.next;
+            head = head.next;
         }
         return true;
+        
+    }
+    ListNode reverse(ListNode head){
+        if(head == null || head.next == null){
+            return head;
+        }
+        ListNode newHead = reverse(head.next);
+        ListNode front = head.next;
+        front.next = head;
+        head.next = null;
+        return newHead;
     }
 }
