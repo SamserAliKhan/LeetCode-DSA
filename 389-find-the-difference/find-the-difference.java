1f07c73ba1a317fd15a915ed6e555c68e4f4648a
Samser Ali Khan <111233312+SamserAliKhan@users.noreply.github.com>
2025-09-07 06:21:42 +0530

    Time: 2 ms (62.59%) | Memory: 42 MB (46.91%) - LeetSync

--- 389-find-the-difference/find-the-difference.java
+++ 389-find-the-difference/find-the-difference.java
@@ -2,7 +2,7 @@ class Solution {
     public char findTheDifference(String s, String t) {
         char ch = 0;
         for(int i = 0 ; i<s.length(); i++){
-            ch = (char)(ch ^ s.charAt(i));
+            ch ^= s.charAt(i);
         }
         for(int i = 0 ; i<t.length(); i++){
             ch = (char)(ch ^ t.charAt(i));
